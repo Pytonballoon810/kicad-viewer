4751ad761f9064d4ad3a2c1f386cd03594472462
Philipp Hofmann <philipp@hofmann-ebs.de>
2025-05-30 15:55:06 +0200

    Update installed.php to reflect the correct reference version

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'pytonballoon810/kicad_viewer',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '28c2dc0c1d7840c53f4366a797ef3fdfac3a85ca',
+        'reference' => '9690604678865fb33f431df2bab15aee9eebda8e',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'pytonballoon810/kicad_viewer' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '28c2dc0c1d7840c53f4366a797ef3fdfac3a85ca',
+            'reference' => '9690604678865fb33f431df2bab15aee9eebda8e',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
